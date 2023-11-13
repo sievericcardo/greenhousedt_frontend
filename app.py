060e6c67096772ab79c2b2460cb9a8a36cb0051c
Riccardo Sieve <sievericcardo@gmail.com>
2023-11-13 22:16:31 +0100

    Added code for call to fuseki

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
+import requests
 from io import BytesIO
 import base64
 
@@ -51,12 +52,34 @@ def clean_up(response):
     plt.close('all')
     return response
 
-@app.after_request
-def clean_up(response):
-    # Clear Matplotlib figure after each request
-    plt.clf()
-    plt.close('all')
-    return response
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Get data from the form
+    query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
+            PREFIX ast: <http://www.semanticweb.org/gianl/ontologies/2023/1/sirius-greenhouse#> \
+            PREFIX owl: <http://www.w3.org/2002/07/owl#> \
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
+            INSERT { \
+            ast:basilicum3 rdf:type owl:NamedIndividual , \
+                                    ast:Basilicum ; \
+                        ast:hasIdealMoisture \"60.0\"^^xsd:double ; \
+                        ast:hasPlantId \"5\"^^xsd:string . \
+            } \
+            WHERE { \
+            FILTER NOT EXISTS { ast:basilicum3 rdf:type ast:Basilicum . \
+            } \
+            } "
+    
+    data = {'update': query}
+    # make a post request to http://localhost:3030/GreenHouse/update
+    r = requests.post('http://localhost:3030/GreenHouse/update', data=data)
+
+    # Process the data if needed
+    result = f"Query submitted: {query}"
+
+    # Return a JSON response
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
